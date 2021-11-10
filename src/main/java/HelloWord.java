e831eb985a196da02e97770d6c529f6df1a8677d
yutao <lirl@itcastn>
2021-11-10 14:50:22 +0800

    Hello4

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("HelloWord1");
+        System.out.println("王佳乐是sb");
     }
 }
