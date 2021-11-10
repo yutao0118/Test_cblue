5459978b95f9241b295b3a738cbd30c57731fca1
yutao <lirl@itcastn>
2021-11-10 14:49:40 +0800

    Hello3

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("HelloWord1");
+        System.out.println("HelloWord2");
+        System.out.println("HelloWord3");
     }
 }
